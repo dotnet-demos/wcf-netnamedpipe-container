5053cdc37b8cc2ac8bc432bd2041232bd3965720
Joy George Kunjikkuru <joymon@gmail.com>
2022-04-08 02:24:32 -0400

    Made net.pipe WCF working inside container

--- commands.ps1
+++ commands.ps1
@@ -1,2 +1,6 @@
 ﻿docker run --name testwc -p 82:80 -d mcr.microsoft.com/dotnet/framework/wcf:4.8-windowsservercore-ltsc2019
 docker run --name testwcf -p 82:80 -d wcf:latest
+
+# TRuublehsoting
+ get-item "IIS:/sites/Default web site"
+ Get-ItemProperty "IIS:/sites/Default web site/InternalService" -name enabledprotocols
